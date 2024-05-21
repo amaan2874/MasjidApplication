2a090c13fb3d717a94f0897e669bb4a774c9d418
farhan2874 <amaanraza4905@gmail.com>
2024-05-21 15:56:26 +0530

    ready pages

--- components/Login.js
+++ components/Login.js
@@ -1,42 +1,26 @@
 import React from "react";
-import {StyleSheet, Text,TextInput,View,Button, Pressable} from 'react-native'
+import {StyleSheet, Text,View, Pressable} from 'react-native'
+import Input from "./Input";
 
-const Login =()=>{
+const Login =({navigation})=>{
     return(
         <View>
             <Text style={{backgroundColor:'#019863',alignItems:'center',alignContent:'center',padding:30,marginTop:100,marginBottom:5,borderRadius:500,height:200,width:200,alignSelf:'center',fontSize:80,color:'white',paddingTop:45,textShadowOffset:{width:8,height:5},textDecorationColor:'black',textShadowRadius:3}}>IFM</Text>
 
-            <TextInput
-            style={styles.TextInput} 
-            placeholder='User Name'>
-            </TextInput>
-            <TextInput
-            style={styles.TextInput} 
-            placeholder='Password'>
-            </TextInput>
+            <Input placeholder="Username"keyboard="default"/>
+            <Input placeholder="Password"keyboard="default" is_password={true}/>
 
             <Text style={{color:'#019863',padding:15,paddingStart:220,fontSize:15}}>Forget Password ?</Text>
 
             <Pressable>
                 <Text style={styles.Pressable}>Login</Text>
             </Pressable>
-            <Text style={{textAlign:'center',fontSize:15}}>If you don't Have an account <Text style={{color:'#019863'}}>regester</Text> here</Text>
+            <Text style={{textAlign:'center',fontSize:15}}>If you don't Have an account <Text onPress={()=>navigation.navigate('User')} style={{color:'#019863'}}>regester</Text> here</Text>
         </View>
     )
 }
 
 const styles= StyleSheet.create({
-    TextInput:{
-        fontSize:18,
-        color:'black',
-        borderWidth:2,
-        borderColor:'black',
-        margin:10,
-        marginStart:30,
-        marginEnd:30,
-        padding:10,
-        borderRadius:5
-    },
     Pressable:{
         backgroundColor:'#019863',
         color:'white',
